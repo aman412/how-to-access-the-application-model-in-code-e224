94a6e86a7d36a438ad67823f80bbf5e64e507b3e
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/HowToAccessApplicationModelInCode.Module.Win/WinModule.Designer.cs
@@ -0,0 +1,37 @@
+namespace HowToAccessApplicationModelInCode.Module.Win {
+   partial class HowToAccessApplicationModelInCodeWindowsFormsModule {
+      /// <summary> 
+      /// Required designer variable.
+      /// </summary>
+      private System.ComponentModel.IContainer components = null;
+
+      /// <summary> 
+      /// Clean up any resources being used.
+      /// </summary>
+      /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+      protected override void Dispose(bool disposing) {
+         if (disposing && (components != null))
+         {
+            components.Dispose();
+         }
+         base.Dispose(disposing);
+      }
+
+      #region Component Designer generated code
+
+      /// <summary> 
+      /// Required method for Designer support - do not modify 
+      /// the contents of this method with the code editor.
+      /// </summary>
+      private void InitializeComponent() {
+         // 
+         // HowToAccessApplicationModelInCodeWindowsFormsModule
+         // 
+         this.RequiredModuleTypes.Add(typeof(HowToAccessApplicationModelInCode.Module.HowToAccessApplicationModelInCodeModule));
+         this.RequiredModuleTypes.Add(typeof(DevExpress.ExpressApp.Win.SystemModule.SystemWindowsFormsModule));
+
+      }
+
+      #endregion
+   }
+}
---
+++ Readme.md
@@ -0,0 +1,9 @@
+# How to Access the Application Model in Code
+
+
+<p>This example demonstrates how to use the View.Model property to set the required value for the current ListView node's MasterDetailMode property. For this purpose, a new View Controller is created, and a SimpleAction is added to it. This Action allows end-users to switch between two modes: the current List View only and the current List View together with the corresponding Detail View.</p>
+<p>See code in the SwitchController.cs (SwitchController.vb) file. For details, refer to the <a href="http://documentation.devexpress.com/#Xaf/CustomDocument2810">Access the Application Model in Code</a> topic in XAF documentation.</p>
+
+<br/>
+
+
