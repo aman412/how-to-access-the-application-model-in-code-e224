6e365c57254920c62e3d2878981a5e7f3b8ca94f
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ VB/HowToAccessApplicationModelInCode.Module.Win/SelectViewController.vb
@@ -0,0 +1,39 @@
+Imports Microsoft.VisualBasic
+Imports System
+Imports System.ComponentModel
+Imports System.Collections.Generic
+Imports System.Diagnostics
+Imports System.Text
+
+Imports DevExpress.ExpressApp
+Imports DevExpress.ExpressApp.Actions
+Imports DevExpress.Persistent.Base
+Imports DevExpress.ExpressApp.Model
+
+Namespace HowToAccessApplicationModelInCode.Module.Win
+	Partial Public Class SelectViewController
+		Inherits ViewController
+		Public Sub New()
+			InitializeComponent()
+			RegisterActions(components)
+		End Sub
+		Private Sub SelectViewAction_Execute(ByVal sender As Object, ByVal e As SingleChoiceActionExecuteEventArgs) Handles SelectViewAction.Execute
+            View.SetInfo(CType(e.SelectedChoiceActionItem.Data, IModelObjectView))
+		End Sub
+		Protected Overrides Sub OnActivated()
+			MyBase.OnActivated()
+			SelectViewAction.BeginUpdate()
+			SelectViewAction.Items.Clear()
+			For Each node As IModelView In View.Model.Application.Views.GetNodes(Of IModelListView)()
+                If TryCast(node, IModelObjectView).ModelClass Is TryCast(View.Model, IModelObjectView).ModelClass Then
+                    Dim current As New ChoiceActionItem(node.Id, node)
+                    SelectViewAction.Items.Add(current)
+                    If node Is View.Model Then
+                        SelectViewAction.SelectedItem = current
+                    End If
+                End If
+			Next node
+			SelectViewAction.EndUpdate()
+		End Sub
+	End Class
+End Namespace
---
+++ VB/HowToAccessApplicationModelInCode.Module/Updater.vb
@@ -0,0 +1,34 @@
+Imports Microsoft.VisualBasic
+Imports System
+
+Imports DevExpress.ExpressApp.Updating
+Imports DevExpress.Xpo
+Imports DevExpress.Data.Filtering
+Imports DevExpress.ExpressApp
+Imports DevExpress.Persistent.BaseImpl
+
+Namespace HowToAccessApplicationModelInCode.Module
+   Public Class Updater
+	   Inherits ModuleUpdater
+	  Public Sub New(ByVal objectSpace As ObjectSpace, ByVal currentDBVersion As Version)
+		  MyBase.New(objectSpace, currentDBVersion)
+	  End Sub
+	  Public Overrides Sub UpdateDatabaseAfterUpdateSchema()
+		 MyBase.UpdateDatabaseAfterUpdateSchema()
+		 If ObjectSpace.FindObject(Of Contact)(CriteriaOperator.Parse("FirstName == 'Mary' && LastName == 'Tellitson'")) Is Nothing Then
+			 Dim contact As Contact = ObjectSpace.CreateObject(Of Contact)()
+			 contact.FirstName = "Mary"
+			 contact.LastName = "Tellitson"
+			 contact.Email = "tellitson@example.com"
+			 contact.Save()
+		 End If
+		 If ObjectSpace.FindObject(Of Contact)(CriteriaOperator.Parse("FirstName == 'Andrew' && LastName == 'Fuller'")) Is Nothing Then
+			 Dim contact As Contact = ObjectSpace.CreateObject(Of Contact)()
+			 contact.FirstName = "Andrew"
+			 contact.LastName = "Fuller"
+			 contact.Email = "fuller@example.com"
+			 contact.Save()
+		 End If
+	  End Sub
+   End Class
+End Namespace
